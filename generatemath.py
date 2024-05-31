a0885552289902fb31490d49980dda2fcbe33bbb
ADM228 <91823517+ADM228@users.noreply.github.com>
2024-05-31 17:05:06 +0300

    MULTIPLICATION MOTHERFU-

--- generatemath.py
+++ generatemath.py
@@ -10,8 +10,8 @@ def generate_equation(radicalAmount: int, featureCount : int, xLimits : tuple = 
     x : int = random.randint(xLimits[0], xLimits[1])
     subX : int = x
     radicalList = []
-    i = radicalAmount
-    while (i > 0):
+    radicalNum = radicalAmount
+    while (radicalNum > 0):
         coefficient = random.randint(coefLimits[0], coefLimits[1])
         action = random.randint(0, featureCount)
         match action:   # addition
@@ -31,9 +31,24 @@ def generate_equation(radicalAmount: int, featureCount : int, xLimits : tuple = 
                 if (coefficient == 0):
                     continue
                 subX += coefficient
+            case 2: # multiplication
+                if (subX < subXLimits[0]):  # we can't fucking divide any more m8
+                    continue
+                coefficients = []
+                for i in range(coefLimits[0], coefLimits[1]+1):
+                    if i == 0 or i == 1 or i == subX or subX // i < subXLimits[0] or subX // i > subXLimits[1]:
+                        continue
+                    if (subX % i == 0):
+                        coefficients.append(i)
+                if (len(coefficients) == 0):
+                    continue
+                diff = [coefficient-i for i in coefficients]
+                diff = diff.index(min(diff))
+                coefficient = coefficients[diff]
+                subX = int(subX // coefficient)
         radicalList.append((action, coefficient))
         
-        i -= 1
+        radicalNum -= 1
     problemStr = f"{subX}"
     radicalList.reverse()
     for i in radicalList:
@@ -42,8 +57,10 @@ def generate_equation(radicalAmount: int, featureCount : int, xLimits : tuple = 
                 problemStr = f"{problemStr}+{i[1]}"
             case 1:
                 problemStr = f"{problemStr}-{i[1]}"
+            case 2:
+                problemStr = f"({problemStr})*{i[1]}"
     
     return problemStr, x
 
 if __name__ == "__main__":
-    print(generate_equation(3, 1))
+    print(generate_equation(5, 2))
