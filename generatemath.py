b04c7c701fa2298542efe10610157479a7dfa261
ADM228 <91823517+ADM228@users.noreply.github.com>
2024-05-31 16:57:33 +0300

    fixed a fatal flaw (important later)

--- generatemath.py
+++ generatemath.py
@@ -4,39 +4,44 @@ from data.examples import Examples
 
 # constants
 opstring = "+-*/"
-subxreplacement = "__SUBX__"
 
 def generate_equation(radicalAmount: int, featureCount : int, xLimits : tuple = (0, 100), coefLimits : tuple = (0, 100), subXLimits : tuple = (1, 100)):
    
     x : int = random.randint(xLimits[0], xLimits[1])
     subX : int = x
-    problemStr = subxreplacement
+    radicalList = []
     i = radicalAmount
     while (i > 0):
         coefficient = random.randint(coefLimits[0], coefLimits[1])
         action = random.randint(0, featureCount)
-        if (action == 0):   # addition
-            if (subX < subXLimits[0]):  # we can't fucking subtract any more m8
-                continue
-            while (subX - coefficient < subXLimits[0]):
-                coefficient = int(coefficient // random.uniform(0.4, 4))
-            if (coefficient == 0):
-                continue
-            subX -= coefficient
-            problemStr = f"{problemStr}+{coefficient}"
-        elif (action == 1): # subtraction
-            if (subX > subXLimits[1]):  # we can't fucking add any more m8
-                continue
-            while (subX + coefficient > subXLimits[1]):
-                coefficient = int(coefficient // random.uniform(0.4, 4))
-            if (coefficient == 0):
-                continue
-            subX += coefficient
-            problemStr = f"{problemStr}-{coefficient}"
+        match action:   # addition
+            case 0:
+                if (subX < subXLimits[0]):  # we can't fucking subtract any more m8
+                    continue
+                while (subX - coefficient < subXLimits[0]):
+                    coefficient = int(coefficient // random.uniform(0.4, 4))
+                if (coefficient == 0):
+                    continue
+                subX -= coefficient
+            case 1: # subtraction
+                if (subX > subXLimits[1]):  # we can't fucking add any more m8
+                    continue
+                while (subX + coefficient > subXLimits[1]):
+                    coefficient = int(coefficient // random.uniform(0.4, 4))
+                if (coefficient == 0):
+                    continue
+                subX += coefficient
+        radicalList.append((action, coefficient))
         
         i -= 1
-    idx = problemStr.find(subxreplacement)
-    problemStr = f"{problemStr[:idx]}{subX}{problemStr[idx+len(subxreplacement):]} = ?"
+    problemStr = f"{subX}"
+    radicalList.reverse()
+    for i in radicalList:
+        match i[0]:
+            case 0:
+                problemStr = f"{problemStr}+{i[1]}"
+            case 1:
+                problemStr = f"{problemStr}-{i[1]}"
     
     return problemStr, x
 
