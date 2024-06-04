1e690749be56a1aa98245bd8c7f1b04edcd85e5c
FlamyMan <99559351+FlamyMan@users.noreply.github.com>
2024-06-04 17:16:47 +0300

    IT IS BISCUTS!!!!

--- static/scripts/type_selection.js
+++ static/scripts/type_selection.js
@@ -1,5 +1,5 @@
-const CALC = '<option value="no" selected >Не выбрано</option><option value="only +">Только +</option><option value="only -">Только -</option><option value="only *">Только *</option><option value="only /">Только /</option>';
-const EQUATION = '<option value="no" selected >Не выбрано</option><option value="only squared">Только ax^2+bx+c</option><option value="only linear">Только kx+b</option>';
+const CALC = '<option value="no" >Не выбрано</option><option value="plus">Только +</option><option value="minus">Только -</option><option value="multi">Только *</option><option value="division">Только /</option>';
+const EQUATION = '<option value="no" >Не выбрано</option><option value="squared">Только ax^2+bx+c</option><option value="lin">Только kx+b</option>';
 
 const VAL_TO_PTYPE = ['calc', 'equation', 'inequality']
 
@@ -11,9 +11,17 @@ var add = document.getElementById("addition");
 
 type.addEventListener("change", render_additional);
 
-let cookie = document.cookie.split('; ');
-
+let pairs = document.cookie.split('; ');
+var cookies = {};
+for (var i=0; i<pairs.length; i++){
+var pair = pairs[i].split("=");
+cookies[(pair[0]+'').trim()] = unescape(pair.slice(1).join('='));
+}
+type.value = VAL_TO_PTYPE[cookies["P_TYPE"]];
+hard.value = VAL_TO_HARD[cookies["HARDNESS"]];
 render_additional();
+add.value = cookies["ADDITIONAL"];
+console.log(cookies["ADDITIONAL"]);
 function render_additional() {
     if (type.value == "calc") 
     {
